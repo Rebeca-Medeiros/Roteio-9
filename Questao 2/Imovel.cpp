6069dceaf6cd33d4e5f31f4aa87ca8aee4cee228
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-10-24 11:08:17 -0300

    Add files via upload

---
+++ Questao 2/Imovel.cpp
@@ -0,0 +1,12 @@
+#include "Imovel.h"
+#include <iostream>
+
+using namespace std;
+
+void Imovel::setEndereco(string ed){
+    endereco = ed;
+}
+
+string Imovel::getEndereco(){
+    return endereco;
+}
--- Questao 2/Imovel.h
+++ Questao 2/Imovel.h
@@ -1,1 +1,18 @@
+#ifndef IMOVEL_H
+#define IMOVEL_H
 
+#include <iostream>
+
+class Imovel{
+    private:
+        std::string endereco;
+    public:
+        void setEndereco(std::string ed);
+        std::string getEndereco();
+
+        virtual void getDescricao() = 0;
+};
+
+
+
+#endif // IMOVEL_H
